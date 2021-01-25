bc074d7c14753d6879bf8e1892c24e1b2590c636
Denny Trebbin <denny.trebbin@gmail.com>
2021-01-25 12:34:34 +0100

    fix(tasks): remove rescript references

--- tasks/package_extension.sh
+++ tasks/package_extension.sh
@@ -2,16 +2,22 @@
 
 const { join } = require('path')
 const { writeFile: write_file } = require('fs/promises')
+const { promisify } = require('util')
+const exec_file = promisify(require('child_process').execFile)
 
 const [, , ...arguments] = process.argv
 const re_arguments = /(?:\s*)(?<key>\S+)(?:\s*)(?:(?<assignment>:)|(?<copy>>))(?:\s*)(?<value>\S+)(?:\s*)|^(?<file_or_folder>.+)$/
 
 const compile = async () => {
   console.log('compiling...')
-  const {stdout} = await exec_file('npm', ['run', 'build'])
-  console.log(stdout, '...done')
+  try {
+    const { stdout } = await exec_file('npm', ['run', 'build'])
+    console.log(stdout, '...done')
+  } catch (err) {
+    console.error(err, '...failed')
+    process.exit(err.code || 1)
+  }
 }
-await compile()
 
 const package_extension = async () => {
   let package_target = '.'
@@ -32,18 +38,13 @@ const package_extension = async () => {
   const package_json = require(package_file)
   const extension_file = join(project_path, 'extension.json')
   const extension_json = require(extension_file)
-  const bsconfig_file = join(project_path, 'bsconfig.json')
-  const bsconfig_json = require(bsconfig_file)
   const extension_file2 = join(project_path, package_target, 'extension.json')
 
   extension_json.version = package_json.version
-  bsconfig_json.version = package_json.version
   extension_json.description = package_json.description
-  bsconfig_json.name = package_json.name
 
   try {
     write_file(extension_file, JSON.stringify(extension_json, null, 2))
-    write_file(bsconfig_file, JSON.stringify(bsconfig_json, null, 2))
     try {
       const extension_json2 = require(extension_file2)
       write_file(extension_file2, JSON.stringify(extension_json, null, 2))
@@ -55,4 +56,8 @@ const package_extension = async () => {
     process.exit(1)
   }
 }
-await package_extension()
+
+(async () => {
+  await compile()
+  await package_extension()
+})()
